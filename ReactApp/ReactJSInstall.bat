94750fe1c276da066af7075794219b56d0da142d
emerson.butzen <emerson.butzen@photoweb.fr>
2020-06-05 23:14:25 +0100

    First class: prepare environment and create React application

---
+++ ReactApp/ReactJSInstall.bat
@@ -0,0 +1,16 @@
+npm init -y
+npm install react --save
+npm install react-dom --save
+npm install webpack --save
+npm install webpack-dev-server --save
+npm install webpack-cli --save
+npm install babel-core --save-dev
+npm install babel-loader@7 --save-dev
+npm install babel-preset-env --save-dev
+npm install babel-preset-react --save-dev
+npm install html-webpack-plugin --save-dev
+touch index.html
+touch App.js
+touch main.js
+touch webpack.config.js
+touch .babelrc
