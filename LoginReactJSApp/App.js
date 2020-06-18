69216e89feabf732684b2fb308b0792a25150151
emerson.butzen <emerson.butzen@photoweb.fr>
2020-06-18 18:08:35 +0100

    Implementation of the third class.

--- LoginReactJSApp/App.js
+++ LoginReactJSApp/App.js
@@ -4,19 +4,32 @@ import reactDOM from 'react-dom';
 class App extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {};
+		this.state = { isLoginOpen: true, isRegisterOpen: false };
+	}
+	
+	showLoginBox() {
+		this.setState({isLoginOpen:true, isRegisterOpen: false });
+	}
+	
+	showRegisterBox() {
+		this.setState({isRegisterOpen:true, isLoginOpen: false });
 	}
 	
 	render() {
 		return(
 			<div className="root-container">
-				<div className="box-container">
-					<div className="controller">
+				<div className="box-controller">
+					<div className="controller" onClick={this.showLoginBox.bind(this)}>
 						Login
 					</div>
-					<div className="controller">
+					<div className="controller" onClick={this.showRegisterBox.bind(this)}>
 						Register
 					</div>
+				</div>
+				
+				<div className="box-container">
+					{this.state.isLoginOpen && <LoginBox />}
+					{this.state.isRegisterOpen && <RegisterBox />}
 				</div>
 			</div>
 		);
--- LoginReactJSApp/webpack.config.js
+++ LoginReactJSApp/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
    },
    devServer: {
       inline: true,
-      port: 5011
+      port: 8001
    },
    module: {
       rules: [
